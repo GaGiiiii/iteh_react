1595a61dc938964cbc97ac8945358a353f2fa5fe
GaGiiiii <dragoslav.gagi8@gmail.com>
2021-03-03 19:36:14 +0100

    Done.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,21 @@
 import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 
-const Example = ({onSearch}) => {
+const Example = ({ onSearch, hasSearch }) => {
   return (
     <>
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">Car Sales</Navbar.Brand>
+        <Navbar.Brand href="/">Car Sales</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Contact</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
           </Nav>
-          <Form inline>
-            <FormControl onChange={(e) => {onSearch(e.target.value)}} type="text" placeholder="Search" className="mr-sm-2" />
+          {hasSearch ? <Form inline>
+            <FormControl onChange={(e) => { onSearch(e.target.value) }} type="text" placeholder="Search" className="mr-sm-2" />
             <Button variant="outline-success">Search</Button>
-          </Form>
+          </Form> : ''}
+
         </Navbar.Collapse>
       </Navbar>
     </>
